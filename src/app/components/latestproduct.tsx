805556cb14b8a836f56e3d0dd822f360e6015c02
sharjeel123456789 <m.sharjeel1244@gmail.com>
2025-01-03 21:24:32 +0500

    Fix dependencies conflict

--- src/app/components/latestproduct.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Latestproduct = () => {
-  return (
-    <div>Latestproduct</div>
-  )
-}
-
-export default Latestproduct
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Hero from "./components/hero";
-import Latestproduct from "./components/latestproduct";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
-      <Latestproduct/>
+      
     </div>
   );
 }
